6aa51a43a5b407049449d81e62f143510d50bdc9
terry-man-com <terry.man.com@icloud.com>
2022-03-26 16:16:26 +0900

    oop課題２途中

---
+++ oop_2/Taxi.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__ . "/Car.php";
+
+class Taxi extends Car
+{
+    private $name;
+    private $number;
+    private $color;
+    public $passenger;
+
+    public function __construct($name, $number, $color)
+    {
+        $this->name = $name;
+        $this->number = $number;
+        $this->color = $color;
+    }
+
+    public function pickUp($passenger)
+    {
+        $this->passenger = strval($passenger);
+        return $this->passenger . "人乗車しました" . "\n";
+        $this->passenger = $this->passenger + $passenger;
+    }
+
+    public function lower($passenger)
+    {
+        if($this->passenger - $passenger >= 0)
+        {
+            return $passenger . "人降車しました\n";
+        } else {
+            return $passenger . "人降車できません\n";
+        }
+    }
+
+    public function information()
+    {
+        return "車の車種：" . $this->name . "\n" .
+            "車体番号：" . $this->number . "\n" .
+            "カラー：" . $this->color . "\n" .
+            "乗車人数：" . $this->passenger . "人\n";
+    }
+
+}
